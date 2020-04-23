4084d7187168abdcc69a5faa7a9c170ea5c3789a
Ian <ian3924@yahoo.co.uk>
2020-04-23 22:31:12 +0100

    Revised version
    Revised version of the script adding in a couple more SQL statements.

--- Get_FileInfo.sql
+++ Get_FileInfo.sql
@@ -5,11 +5,12 @@
 --  for a clone (copy) of a database when you have to remove
 --  these files.
 --
---  Last updated: 19 August 2016 14:51:40
+--  Last updated: 02 September 2016 15:10:00
 --------------------------------------------------------
 set heading on
 set feedback on
 set pagesize 1000
+
 
 --------------------------------------------------------
 --  Instance information
@@ -43,6 +44,17 @@ SELECT file_id,
 FROM   dba_data_files;
 
 
+-- A distinct listing of the above.
+with positions
+as (
+  select file_name,
+      instr(file_name, '/', -1) as pos1,
+      instr(file_name, '.', -1) as pos2
+  from dba_data_files )
+select distinct REPLACE(t1.file_name, substr(t1.file_name, t2.pos1, (t2.pos2-t2.pos1)), '/*') as distinct_paths
+from dba_data_files t1, positions t2
+where t1.file_name = t2.file_name;
+
 --------------------------------------------------------
 --  Get online redo log information.
 --------------------------------------------------------
@@ -54,6 +66,17 @@ column member format a60 heading "Logfile"
 select group#, member from v$logfile order by group#, member;
 
 
+-- A distinct listing of the above.
+with positions
+as (
+  select member,
+      instr(member, '/', -1) as pos1,
+      instr(member, '.', -1) as pos2
+  from v$logfile )
+select distinct REPLACE(t1.member, substr(t1.member, t2.pos1, (t2.pos2-t2.pos1)), '/*') as distinct_paths
+from v$logfile t1, positions t2
+where t1.member = t2.member;
+
 --------------------------------------------------------
 --  Get controlfile names
 --------------------------------------------------------
@@ -61,7 +84,23 @@ PROMPT
 PROMPT Control files
 PROMPT
 column name format a52
+column SUMMARY_OF_PATHS format a52
 select rownum, name from v$controlfile order by name;
+
+
+-- A distinct listing of the above.
+WITH positions
+     AS (SELECT name,
+                Instr(name, '/', -1) AS pos1,
+                Instr(name, '.', -1) AS pos2
+         FROM   v$controlfile)
+SELECT DISTINCT Replace(t1.name, Substr(t1.name, t2.pos1,
+                ( t2.pos2 - t2.pos1 )), '/*') AS
+                distinct_paths
+FROM   v$controlfile t1,
+       positions t2
+WHERE  t1.name = t2.name;
+
 
 --------------------------------------------------------
 --  Archive log mode currently in use
